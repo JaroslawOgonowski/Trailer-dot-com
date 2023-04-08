44fb334ed41aced0dcc33ace685e37c5934acac4
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-04-08 22:52:27 +0200

    footer change

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -4,7 +4,6 @@ import { FooterContent } from "./styled"
 export const Footer = () => {
   return (
     <FooterContent>
-      The design is a loosely interpreted look for a netflix movie page <br />
       Jarosław Ogonowski 2023
     </FooterContent>
   );
